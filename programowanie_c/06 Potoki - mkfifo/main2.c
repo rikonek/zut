1c6ea2a9b23d3f5b49f641435040056e3f7632e9
Lab User <user@wi.zut.pl>
2017-04-02 11:07:32 +0200

    Dodanie

---
+++ programowanie_c/06 Potoki - mkfifo/main2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define MAX_BUF 1024
+
+int main()
+{
+	int fd;
+	char * myfifo="/tmp/myfifo";
+	char buf[MAX_BUF];
+
+	/* open, read and display the message from the FIFO */
+	fd=open(myfifo,O_RDONLY);
+	read(fd,buf,MAX_BUF);
+	printf("Received: %s\n",buf);
+	close(fd);
+
+	return 0;
+}
