3292f77a9095b238eec3fc960dcec1035f21d05a
Lab User <user@wi.zut.pl>
2017-05-21 00:48:18 +0200

    Aktualizacja programu dominanta

--- programy/dominanta.c
+++ programy/dominanta.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h> //malloc
 
-#define KOMENTARZ 1
+#define POKAZ_KOMENTARZ 1
 
 /*
  * Losuje liczby 0-4
@@ -11,25 +12,26 @@
  */
 int *losuj(int n)
 {
-    static int tablica[n];
+    int *tablica=malloc(sizeof(int) * n);
     int i=0;
 
+    //inicjalizuje tablice wartoscia 0
     memset(tablica,0,sizeof(tablica));
 
-    if(KOMENTARZ) printf("xxxGeneruje losowa tablice liczb - ilosc elementow %d\n",n);
+    if(POKAZ_KOMENTARZ) printf("Generuje losowa tablice liczb - ilosc elementow %d\n",n);
     srand(time(NULL));
     for(;i<n;i++)
     {
-        tablica[i]=rand()%5;
-        if(KOMENTARZ) printf("%d,",tablica[i]);
+        tablica[i]=rand()%10; //losuje liczby z zakresu 0-9
+        if(POKAZ_KOMENTARZ) printf("%d ",tablica[i]);
     }
-    if(KOMENTARZ) printf("\n");
+    if(POKAZ_KOMENTARZ) printf("\n\n");
     return tablica;
 }
 
 int main()
 {
-    int n=5;
+    int n=100;
     int *ptablica; //tablica wylosowanych liczb
     int znalezione[n]; //tablica unikalnych liczb
     int znalezione_ilosc=0; //ilosc zliczonych unikalnych liczb
@@ -47,52 +49,39 @@ int main()
 
     //losuje tablice danych
     ptablica=losuj(n);
-    printf("Wartosc z Tablica: %d",*(ptablica+1));
 
-/*
     //zliczam ilosc poszczegolnych liczb
     for(i=0;i<n;i++)
     {
-        printf("Iteracja: %d, znalezione_ilosc: %d\n",i,znalezione_ilosc);
         jest_w_znalezionych=0;
         for(z=0;z<=znalezione_ilosc;z++)
         {
-            if(*(tablica+i)==znalezione[z])
+            if(*(ptablica+i)==znalezione[z])
             {
-                printf("Znalezione %d",z);
+                jest_w_znalezionych=1;
+                ilosc[z]++;
+
+                //jezeli liczba=0 i ilosc=1 to zwieksz znalezione_ilosc - potrzebne bo tablica jest domyslnie wypelniona 0
+                if(*(ptablica+i)==0 && ilosc[z]==1)
+                {
+                    znalezione_ilosc++;
+                }
+                break;
             }
         }
         if(jest_w_znalezionych==0)
         {
+            znalezione[znalezione_ilosc]=*(ptablica+i);
+            ilosc[znalezione_ilosc]++;
             znalezione_ilosc++;
         }
     }
-*/
-/*
-    //zliczam ilosc poszczegolnych liczb
-    for(i=0;i<n;i++)
+    if(POKAZ_KOMENTARZ)
     {
-//printf("iteracja: %d, znalezione_ilosc: %d\n",i,znalezione_ilosc);
-        jest_w_znalezionych=0;
-        if(znalezione_ilosc>0)
+        printf("Znalezione liczby:\n");
+        for(i=0;i<znalezione_ilosc;i++)
         {
-            for(z=0;z<=znalezione_ilosc;z++)
-            {
-                if(tablica[i]==znalezione[z])
-                {
-                  jest_w_znalezionych=1;
-                  ilosc[z]++;
-    //              printf(" - Z1: %d, ilosc: %d\n",z,ilosc[z]);
-                  break;
-                }
-            }
-        }
-        if(jest_w_znalezionych==0)
-        {
-            znalezione[znalezione_ilosc]=tablica[i];
-            ilosc[znalezione_ilosc]++;
-            znalezione_ilosc++;
-//            printf(" - Z0: %d, ilosc: %d\n",i,tablica[i]);
+            printf("Liczba: %d, ilosc wystapien: %d\n",znalezione[i],ilosc[i]);
         }
     }
 
@@ -122,25 +111,23 @@ int main()
     }
 
     //wyswietlam dominanty
-    if(KOMENTARZ) printf("\n");
+    if(POKAZ_KOMENTARZ) printf("\n");
     if(dominanta_ilosc_wystapien==znalezione_ilosc)
     {
-        printf("Brak dominanty");
+        printf("Brak dominanty\n");
     }
     else
     {
         printf("Znaleziono dominant: %d\n",dominanta_ilosc_wystapien);
-//        printf("Ilosc powtorzen: %d\n",znalezione_ilosc);
-        printf("tj.: ");
         for(i=0;i<znalezione_ilosc;i++)
         {
             if(dominanta_ilosc==ilosc[i])
             {
-                printf("%d,",znalezione[i]);
+                printf(" - Dominanta: %d, ilosc wystapien: %d\n",znalezione[i],ilosc[i]);
             }
         }
+        printf("\n");
     }
-*/
 
     return 1;
 }
